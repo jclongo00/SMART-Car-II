fc5b759932e244f984b8772ecbd0e02ae55dd9e7
jclongo00 <46696145+jclongo00@users.noreply.github.com>
2022-02-22 09:09:56 -0800

    V1.1 of main_controller.py
    Started the initial main controller program and started testing the functionality of the pwm waves for the raspberry pi to control the servo.

---
+++ main_controller.py
@@ -0,0 +1,69 @@
+# File:      main_controller.py
+# Date:      2/15/2022
+# Version:   1.0
+# Motor:
+#   Frequency:     93-93.3Hz
+#   Motor forward: 13% - 16%
+#   Motor Reverse: %
+#   Motor idle:    14.9%
+# Servo:
+#   Frequency:     187.5Hz
+#   Left:          20%
+#   Right:         35%
+#   Idle:          27.5%
+
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+motorPin = 12     # PWM pin connected to Motor
+servoPin = 16     # PWM pin connected to Servo
+
+
+def setup():
+    GPIO.setwarnings(False)  #disable warnings
+    GPIO.setmode(GPIO.BOARD) #set pin numbering system
+    GPIO.setup(motorPin,GPIO.OUT)
+    GPIO.setup(servoPin,GPIO.OUT)
+    print('setup')
+
+#pi_pwm = GPIO.PWM(opin,93.3) #create PWM instance with frequency
+#pi_pwm.start(13.6) 
+
+
+def motor():
+    for dc in range(0, 101, 5):    # Loop 0 to 100 stepping dc by 5 each loop
+        motor_pwm.ChangeDutyCycle(dc)
+        sleep(0.05)             # wait .05 seconds at current LED brightness
+
+def servo():
+    for dc in range(40, 72):    # Loop 20 to 35 stepping dc by .5 each loop
+        servo_pwm.ChangeDutyCycle(dc/2)
+        sleep(0.5)             # wait .05 seconds at current LED brightness
+    for dc in range(70, 38, -1):    # Loop 20 to 35 stepping dc by .5 each loop
+        servo_pwm.ChangeDutyCycle(dc/2)
+        sleep(0.5)             # wait .05 seconds at current LED brightness
+
+
+def loop():
+    #motor()
+    servo()
+    #sleep(.001)
+ 
+def destroy():
+    GPIO.cleanup()
+ 
+if __name__ == '__main__':
+    print('program start')
+    setup()
+    try:
+        motor_pwm = GPIO.PWM(motorPin,93.3) #create PWM instance with frequency
+        motor_pwm.start(14.9)
+        servo_pwm = GPIO.PWM(servoPin,187.5) #create PWM instance with frequency
+        servo_pwm.start(20)
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+
+
+
