8510fdfafa5e09b8c96716a530da485656e8eacb
jclongo00 <46696145+jclongo00@users.noreply.github.com>
2022-02-27 22:47:37 -0800

    Added lights
    Added lights to indicate which program is running

--- userInterface.py
+++ userInterface.py
@@ -6,19 +6,50 @@ class MyPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
         
-        textBox = wx.TextCtrl(self, value = "",style = wx.TE_READONLY|wx.TE_MULTILINE, pos =(250, 300), size =(1400, 300))
+        self.textBox = wx.TextCtrl(self, value = "",style = wx.TE_READONLY|wx.TE_MULTILINE, size =(1000, -1))
         
-        button1 = wx.Button(self, label='Point-to-Point Navigation', pos =(800, 60), size =(300, 40))
+        img = wx.Image("40light_off.jpg")
+        self.image1 = wx.StaticBitmap(self, bitmap=wx.Bitmap(img))
+        self.image2 = wx.StaticBitmap(self, bitmap=wx.Bitmap(img))
+        self.image3 = wx.StaticBitmap(self, bitmap=wx.Bitmap(img))
+        
+        button1 = wx.Button(self, label='Point-to-Point Navigation', size =(300, 40))
         button1.Bind(wx.EVT_BUTTON, self.on_button1)
         
-        button2 = wx.Button(self, label='Adaptive Cruise Control', pos =(800, 120), size =(300, 40))
+        button2 = wx.Button(self, label='Adaptive Cruise Control', size =(300, 40))
         button2.Bind(wx.EVT_BUTTON, self.on_button2)
         
-        button3 = wx.Button(self, label='Auto Parking', pos =(800, 180), size =(300, 40))
+        button3 = wx.Button(self, label='Auto Parking', size =(300, 40))
         button3.Bind(wx.EVT_BUTTON, self.on_button3)
         
-        exitButton = wx.Button(self, label='Exit', pos =(875, 800), size =(150, 40))
+        stopButton = wx.Button(self, label='Stop', size =(300, 40))
+        stopButton.Bind(wx.EVT_BUTTON, self.on_stopButton)
+        
+        exitButton = wx.Button(self, label='Exit', size =(150, 40))
         exitButton.Bind(wx.EVT_BUTTON, self.on_exitButton)
+        
+        main_Sizer = wx.BoxSizer(wx.VERTICAL)
+        hsizer1 = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer3 = wx.BoxSizer(wx.HORIZONTAL)
+        
+        hsizer1.Add(self.image1, 0, wx.ALL, 5)
+        hsizer1.Add(button1, 0, wx.ALL, 5)
+        main_Sizer.Add(hsizer1, 0, wx.ALIGN_CENTER)
+        
+        hsizer2.Add(self.image2, 0, wx.ALL, 5)
+        hsizer2.Add(button2, 0, wx.ALL, 5)
+        main_Sizer.Add(hsizer2, 0, wx.ALIGN_CENTER)
+        
+        hsizer3.Add(self.image3, 0, wx.ALL, 5)
+        hsizer3.Add(button3, 0, wx.ALL, 5)
+        main_Sizer.Add(hsizer3, 0, wx.ALIGN_CENTER)
+
+        main_Sizer.Add(self.textBox, 0, wx.ALIGN_CENTER, 5)
+        main_Sizer.Add(stopButton, 0, wx.ALIGN_CENTER, 5)
+        main_Sizer.Add(exitButton, 0, wx.ALIGN_CENTER, 5)
+        
+        self.SetSizer(main_Sizer)
         
         #textBox.SetValue('Starting Point-to-Point Navigation Module...')
         #textBox.SetValue('Starting Adaptive Cruise Control Module...')
@@ -26,16 +57,39 @@ class MyPanel(wx.Panel):
         
     
     def on_button1(self, event):
-        print('Starting Point-to-Point Navigation Module')
-        textBox = wx.TextCtrl(self, value = "Starting Point-to-Point Navigation Module...",style = wx.TE_READONLY|wx.TE_MULTILINE, pos =(250, 300), size =(1400, 300))
+        self.textBox.SetValue('Starting Point-to-Point Navigation Module...')
+        off_img = wx.Image("40light_off.jpg")
+        on_img = wx.Image("40light_on.jpg")
+        self.image1.SetBitmap(wx.Bitmap(on_img))
+        self.image2.SetBitmap(wx.Bitmap(off_img))
+        self.image3.SetBitmap(wx.Bitmap(off_img))
+        self.Refresh()
     
     def on_button2(self, event):
-        print('Starting Adaptive Cruise Control Module')
-        textBox = wx.TextCtrl(self, value = "Starting Adaptive Cruise Control Module...",style = wx.TE_READONLY|wx.TE_MULTILINE, pos =(250, 300), size =(1400, 300))
+        self.textBox.SetValue('Starting Adaptive Cruise Control Module...')
+        off_img = wx.Image("40light_off.jpg")
+        on_img = wx.Image("40light_on.jpg")
+        self.image1.SetBitmap(wx.Bitmap(off_img))
+        self.image2.SetBitmap(wx.Bitmap(on_img))
+        self.image3.SetBitmap(wx.Bitmap(off_img))
+        self.Refresh()
         
     def on_button3(self, event):
-        print('Starting Auto Parking Module')
-        textBox = wx.TextCtrl(self, value = "Starting Auto Parking Module...",style = wx.TE_READONLY|wx.TE_MULTILINE, pos =(250, 300), size =(1400, 300))
+        self.textBox.SetValue('Starting Auto Parking Module...')
+        off_img = wx.Image("40light_off.jpg")
+        on_img = wx.Image("40light_on.jpg")
+        self.image1.SetBitmap(wx.Bitmap(off_img))
+        self.image2.SetBitmap(wx.Bitmap(off_img))
+        self.image3.SetBitmap(wx.Bitmap(on_img))
+        self.Refresh()
+        
+    def on_stopButton(self, event):
+    	self.textBox.SetValue('Stopping...')
+    	off_img = wx.Image("40light_off.jpg")
+    	self.image1.SetBitmap(wx.Bitmap(off_img))
+    	self.image2.SetBitmap(wx.Bitmap(off_img))
+    	self.image3.SetBitmap(wx.Bitmap(off_img))
+    	self.Refresh()
         
     def on_exitButton(self, event):
         self.GetParent().Close()
